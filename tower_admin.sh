895cf334458fefe0cd3e06f1fd627cbe0a76d258
mbhein <matthew.hein@gmail.com>
2018-08-29 08:05:38 -0500

    set tower admin password

--- tower_admin.sh
+++ tower_admin.sh
@@ -4,5 +4,10 @@ set admin_pwd "ansiblefest"
 
 #Set Tower admin password
 spawn tower-manage changepassword admin
+expect "Changing password for user 'admin'"
 expect "Password:"
-send "$admin_pwd"
+send "$admin_pwd\r"
+expect "Password (again):"
+send "$admin_pwd\r"
+expect "Password changed successfully for user 'admin'"
+expect eof
