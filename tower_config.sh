85a06080a0c3298448a6e3e964004a9a20481dda
mbhein <matthew.hein@gmail.com>
2018-08-29 08:14:59 -0500

    create vagrant cred

--- tower_config.sh
+++ tower_config.sh
@@ -2,13 +2,11 @@
 
 admin_pwd='ansiblefest'
 
-#Set Tower admin password
-spawn tower-manage changepassword admin
-expect "password: "
-send "$admin_pwd"
+
 #Set tower-cli
 tower-cli config host 192.168.80.20
 tower-cli config username admin
 tower-cli config password $admin_pwd
 tower-cli config verify_ssl false
 # TODO tower-cli credential create machine vagrant password vagrant
+tower-cli credential create -n vagrant --credential-type="Machine" --organization Default --insecure --inputs='{"username":"vagrant","password":"vagrant"}'
